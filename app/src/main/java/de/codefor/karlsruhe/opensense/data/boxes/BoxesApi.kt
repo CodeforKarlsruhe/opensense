cfc46f7ae45f226534aa69cf2ec58b402dc1631e
Daniel Bälz <me@dbaelz.de>
2019-03-14 00:19:35 +0100

    Update code based on updated library and REST apis

--- app/src/main/java/de/codefor/karlsruhe/opensense/data/boxes/BoxesApi.kt
+++ app/src/main/java/de/codefor/karlsruhe/opensense/data/boxes/BoxesApi.kt
@@ -5,13 +5,14 @@ import de.codefor.karlsruhe.opensense.data.boxes.model.SensorHistory
 import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface BoxesApi {
     @GET("boxes/{boxId}")
     fun getBox(@Path("boxId") boxId: String) : Single<SenseBox>
 
     @GET("boxes")
-    fun getAllBoxes(): Single<List<SenseBox>>
+    fun getAllBoxes(@Query("minimal") minimal: Boolean = true): Single<List<SenseBox>>
 
     @GET("boxes/{boxId}/data/{sensorId}")
     fun getSensorHistory(@Path("boxId") boxId: String, @Path("sensorId") sensorId: String): Single<List<SensorHistory>>
