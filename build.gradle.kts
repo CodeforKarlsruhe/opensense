ba91f73f4be5d030f7caa74038c576b1edab2c04
Tobias Preuss <tobias.preuss@googlemail.com>
2021-02-26 13:10:11 +0100

    Use gradle-versions-plugin to detect version updates.
    + Adds the "dependencyUpdates" task.
    + See: https://github.com/ben-manes/gradle-versions-plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
+apply(plugin = "com.github.ben-manes.versions")
+
 buildscript {
     repositories {
         google()
@@ -8,6 +10,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:4.1.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${de.codefor.karlsruhe.opensense.build.BuildConfig.kotlinVersion}")
+        classpath("com.github.ben-manes:gradle-versions-plugin:0.36.0")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
